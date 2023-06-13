0dc6cdd9d8530c1e02c3271b61a4f854eb6c6bd5
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2023-06-13 18:07:41 -0400

    Working

--- survey-js-form-card.js
+++ survey-js-form-card.js
@@ -3,7 +3,6 @@ import {
   html,
   css,
 } from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
-import "https://unpkg.com/nouislider/dist/nouislider.min.js";
 import "https://unpkg.com/jquery";
 
 function loadCSS(url) {
@@ -15,6 +14,18 @@ function loadCSS(url) {
 }
 
 loadCSS("https://unpkg.com/survey-jquery@1.9.84/defaultV2.min.css");
+
+const customCss1 = {
+  boolean: {
+    item: "boolean",
+  },
+};
+
+const customCss2 = {
+  boolean: {
+    item: "sd-boolean",
+  },
+};
 
 class SurveyCard extends LitElement {
   static get properties() {
@@ -62,11 +73,6 @@ class SurveyCard extends LitElement {
           thisNode.constructSurveyUI();
         }
       );
-      $.getScript(
-        "https://unpkg.com/surveyjs-widgets@1.9.90/surveyjs-widgets.min.js"
-      ).done((script, textStatus) => {
-        console.log("Survey JS Widgets loaded");
-      });
     });
   }
 
@@ -129,18 +135,26 @@ class SurveyCard extends LitElement {
 
   constructSurveyUI() {
     var thisNode = this;
-    window["surveyjs-widgets"].nouislider(Survey);
-    console.log(
-      "Script accesed",
-      Survey,
-      "Config",
-      this.config,
-      noUiSlider,
-      Survey.Serializer.getProperties("nouislider")
-    );
+
+    console.log("Script accesed", Survey, "Config", this.config);
 
     this.survey = new Survey.Model(this.config.surveyjs_json);
-    console.log("Survey Model", this.survey, this.survey.visiblePages);
+    console.log(
+      "Survey Model",
+      this.survey,
+      this.survey.visiblePages,
+      this.survey.currentPageNo
+    );
+
+    console.log(this.config.surveyjs_json);
+
+    this.survey.onUpdateQuestionCssClasses.add(function (_, options) {
+      thisNode.pageCssLogic(options);
+    });
+
+    // survey.onCurrentPageChanged.add(() => {
+    //   updateNavBar();
+    // });
 
     this.survey_state = "received";
 
@@ -168,6 +182,12 @@ class SurveyCard extends LitElement {
             window.location.href = "/";
           };
           setTimeout(() => {
+            // this._hass.callService("input_select.select_option", "idle", {
+            //   entity_id: "input_select.survey_lifecycle",
+            // });
+            // this._hass.callApi("POST", "states/input_select.survey_lifecycle", {
+            //   state: "idle",
+            // });
             window.location.href = "/";
           }, 2000);
         });
@@ -178,8 +198,54 @@ class SurveyCard extends LitElement {
     });
   }
 
+  pageCssLogic(options) {
+    console.log(options, "Custom CSS", options.question.getType());
+
+    for (let ele of this.config.surveyjs_json.pages[this.survey.currentPageNo]
+      .elements) {
+      console.log(
+        "Page details",
+        this.config.surveyjs_json.pages[this.survey.currentPageNo],
+        this.survey.currentPageNo,
+        options.question.fullTitle
+      );
+      if (
+        options.question.getType() == ele.type &&
+        options.question.fullTitle == ele.title &&
+        ele?.customCssClassDetails
+      ) {
+        // this.survey.css = ele?.customCssClassDetails;
+
+        const classes = options.cssClasses;
+        const classKey = Object.keys(
+          Object.values(ele?.customCssClassDetails)[0]
+        )[0];
+        const classValue = Object.values(
+          Object.values(ele?.customCssClassDetails)[0]
+        )[0];
+
+        console.log(classKey, classValue, "Classes");
+
+        classes[classKey] = classValue;
+
+        console.log(
+          "Available",
+          ele.type,
+          this.survey.css,
+          ele?.customCssClassDetails
+        );
+        break;
+      }
+    }
+  }
+
   render() {
-    return html` <div id="surveyElement"></div> `;
+    return html`
+      <style>
+        ${this.config.customCss}
+      </style>
+      <div id="surveyElement"></div>
+    `;
   }
 
   static get styles() {
@@ -187,10 +253,10 @@ class SurveyCard extends LitElement {
     return [
       css`
         /*!
-  * surveyjs - Survey JavaScript library v1.9.83
-  * Copyright (c) 2015-2023 Devsoft Baltic OÜ  - http://surveyjs.io/
-  * License: MIT (http://www.opensource.org/licenses/mit-license.php)
-  */
+    * surveyjs - Survey JavaScript library v1.9.83
+    * Copyright (c) 2015-2023 Devsoft Baltic OÜ  - http://surveyjs.io/
+    * License: MIT (http://www.opensource.org/licenses/mit-license.php)
+    */
         @font-face {
           font-family: "Open Sans";
           font-style: normal;
@@ -5717,277 +5783,6 @@ class SurveyCard extends LitElement {
 
         .sv-popup.sv-popup--overlay .sv-popup__body-content {
           background-color: var(--background-dim, #f3f3f3);
-        }
-      `,
-      css`
-        .noUi-target,
-        .noUi-target * {
-          -webkit-touch-callout: none;
-          -webkit-tap-highlight-color: transparent;
-          -webkit-user-select: none;
-          -ms-touch-action: none;
-          touch-action: none;
-          -ms-user-select: none;
-          -moz-user-select: none;
-          user-select: none;
-          -moz-box-sizing: border-box;
-          box-sizing: border-box;
-        }
-        .noUi-target {
-          position: relative;
-        }
-        .noUi-base,
-        .noUi-connects {
-          width: 100%;
-          height: 100%;
-          position: relative;
-          z-index: 1;
-        }
-        .noUi-connects {
-          overflow: hidden;
-          z-index: 0;
-        }
-        .noUi-connect,
-        .noUi-origin {
-          will-change: transform;
-          position: absolute;
-          z-index: 1;
-          top: 0;
-          right: 0;
-          height: 100%;
-          width: 100%;
-          -ms-transform-origin: 0 0;
-          -webkit-transform-origin: 0 0;
-          -webkit-transform-style: preserve-3d;
-          transform-origin: 0 0;
-          transform-style: flat;
-        }
-        .noUi-txt-dir-rtl.noUi-horizontal .noUi-origin {
-          left: 0;
-          right: auto;
-        }
-        .noUi-vertical .noUi-origin {
-          top: -100%;
-          width: 0;
-        }
-        .noUi-horizontal .noUi-origin {
-          height: 0;
-        }
-        .noUi-handle {
-          -webkit-backface-visibility: hidden;
-          backface-visibility: hidden;
-          position: absolute;
-        }
-        .noUi-touch-area {
-          height: 100%;
-          width: 100%;
-        }
-        .noUi-state-tap .noUi-connect,
-        .noUi-state-tap .noUi-origin {
-          -webkit-transition: transform 0.3s;
-          transition: transform 0.3s;
-        }
-        .noUi-state-drag * {
-          cursor: inherit !important;
-        }
-        .noUi-horizontal {
-          height: 18px;
-        }
-        .noUi-horizontal .noUi-handle {
-          width: 34px;
-          height: 28px;
-          right: -17px;
-          top: -6px;
-        }
-        .noUi-vertical {
-          width: 18px;
-        }
-        .noUi-vertical .noUi-handle {
-          width: 28px;
-          height: 34px;
-          right: -6px;
-          bottom: -17px;
-        }
-        .noUi-txt-dir-rtl.noUi-horizontal .noUi-handle {
-          left: -17px;
-          right: auto;
-        }
-        .noUi-target {
-          background: #fafafa;
-          border-radius: 4px;
-          border: 1px solid #d3d3d3;
-          box-shadow: inset 0 1px 1px #f0f0f0, 0 3px 6px -5px #bbb;
-        }
-        .noUi-connects {
-          border-radius: 3px;
-        }
-        .noUi-connect {
-          background: #3fb8af;
-        }
-        .noUi-draggable {
-          cursor: ew-resize;
-        }
-        .noUi-vertical .noUi-draggable {
-          cursor: ns-resize;
-        }
-        .noUi-handle {
-          border: 1px solid #d9d9d9;
-          border-radius: 3px;
-          background: #fff;
-          cursor: default;
-          box-shadow: inset 0 0 1px #fff, inset 0 1px 7px #ebebeb,
-            0 3px 6px -3px #bbb;
-        }
-        .noUi-active {
-          box-shadow: inset 0 0 1px #fff, inset 0 1px 7px #ddd,
-            0 3px 6px -3px #bbb;
-        }
-        .noUi-handle:after,
-        .noUi-handle:before {
-          content: "";
-          display: block;
-          position: absolute;
-          height: 14px;
-          width: 1px;
-          background: #e8e7e6;
-          left: 14px;
-          top: 6px;
-        }
-        .noUi-handle:after {
-          left: 17px;
-        }
-        .noUi-vertical .noUi-handle:after,
-        .noUi-vertical .noUi-handle:before {
-          width: 14px;
-          height: 1px;
-          left: 6px;
-          top: 14px;
-        }
-        .noUi-vertical .noUi-handle:after {
-          top: 17px;
-        }
-        [disabled] .noUi-connect {
-          background: #b8b8b8;
-        }
-        [disabled] .noUi-handle,
-        [disabled].noUi-handle,
-        [disabled].noUi-target {
-          cursor: not-allowed;
-        }
-        .noUi-pips,
-        .noUi-pips * {
-          -moz-box-sizing: border-box;
-          box-sizing: border-box;
-        }
-        .noUi-pips {
-          position: absolute;
-          color: #999;
-        }
-        .noUi-value {
-          position: absolute;
-          white-space: nowrap;
-          text-align: center;
-        }
-        .noUi-value-sub {
-          color: #ccc;
-          font-size: 10px;
-        }
-        .noUi-marker {
-          position: absolute;
-          background: #ccc;
-        }
-        .noUi-marker-sub {
-          background: #aaa;
-        }
-        .noUi-marker-large {
-          background: #aaa;
-        }
-        .noUi-pips-horizontal {
-          padding: 10px 0;
-          height: 80px;
-          top: 100%;
-          left: 0;
-          width: 100%;
-        }
-        .noUi-value-horizontal {
-          -webkit-transform: translate(-50%, 50%);
-          transform: translate(-50%, 50%);
-        }
-        .noUi-rtl .noUi-value-horizontal {
-          -webkit-transform: translate(50%, 50%);
-          transform: translate(50%, 50%);
-        }
-        .noUi-marker-horizontal.noUi-marker {
-          margin-left: -1px;
-          width: 2px;
-          height: 5px;
-        }
-        .noUi-marker-horizontal.noUi-marker-sub {
-          height: 10px;
-        }
-        .noUi-marker-horizontal.noUi-marker-large {
-          height: 15px;
-        }
-        .noUi-pips-vertical {
-          padding: 0 10px;
-          height: 100%;
-          top: 0;
-          left: 100%;
-        }
-        .noUi-value-vertical {
-          -webkit-transform: translate(0, -50%);
-          transform: translate(0, -50%);
-          padding-left: 25px;
-        }
-        .noUi-rtl .noUi-value-vertical {
-          -webkit-transform: translate(0, 50%);
-          transform: translate(0, 50%);
-        }
-        .noUi-marker-vertical.noUi-marker {
-          width: 5px;
-          height: 2px;
-          margin-top: -1px;
-        }
-        .noUi-marker-vertical.noUi-marker-sub {
-          width: 10px;
-        }
-        .noUi-marker-vertical.noUi-marker-large {
-          width: 15px;
-        }
-        .noUi-tooltip {
-          display: block;
-          position: absolute;
-          border: 1px solid #d9d9d9;
-          border-radius: 3px;
-          background: #fff;
-          color: #000;
-          padding: 5px;
-          text-align: center;
-          white-space: nowrap;
-        }
-        .noUi-horizontal .noUi-tooltip {
-          -webkit-transform: translate(-50%, 0);
-          transform: translate(-50%, 0);
-          left: 50%;
-          bottom: 120%;
-        }
-        .noUi-vertical .noUi-tooltip {
-          -webkit-transform: translate(0, -50%);
-          transform: translate(0, -50%);
-          top: 50%;
-          right: 120%;
-        }
-        .noUi-horizontal .noUi-origin > .noUi-tooltip {
-          -webkit-transform: translate(50%, 0);
-          transform: translate(50%, 0);
-          left: auto;
-          bottom: 10px;
-        }
-        .noUi-vertical .noUi-origin > .noUi-tooltip {
-          -webkit-transform: translate(0, -18px);
-          transform: translate(0, -18px);
-          top: auto;
-          right: 28px;
         }
       `,
     ];
